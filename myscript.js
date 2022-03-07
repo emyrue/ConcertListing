764683b65ab9fceef6dcb73b858f47fb2fabf1ef
emyrue <94935633+emyrue@users.noreply.github.com>
2022-03-07 15:09:38 -0700

    finish popup menu

--- myscript.js
+++ myscript.js
@@ -4,7 +4,7 @@ const markup = `
   <div class="speakers">  
     <div class="row visible">
       <div class="speaker">
-        <img class="speaker-img" src="./images/Speaker1.png">
+        <img class="speaker-img" src="./images/profile.png" alt="first speaker">
         <div class="speaker-info">
           <div class="name lato">Yochai Benkler</div>
           <div class="italics lato">
@@ -17,35 +17,7 @@ const markup = `
         </div>
       </div>
       <div class="speaker">
-        <img class="speaker-img" src="./images/Speaker2.png">
-        <div class="speaker-info">
-          <div class="name lato">Yochai Benkler</div>
-          <div class="italics lato">
-            <em>Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</em>
-          </div>
-          <div class="line2"></div>
-          <div class="background lato">
-            Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row find-row">
-      <div class="speaker">
-        <img class="speaker-img" src="./images/Speaker3.png">
-        <div class="speaker-info">
-          <div class="name lato">Yochai Benkler</div>
-          <div class="italics lato">
-            <em>Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</em>
-          </div>
-          <div class="line2"></div>
-          <div class="background lato">
-            Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.
-          </div>
-        </div>
-      </div>
-      <div class="speaker">
-        <img class="speaker-img" src="./images/Speaker4.png">
+        <img class="speaker-img" src="./images/profile.png" alt="second speaker">
         <div class="speaker-info">
           <div class="name lato">Yochai Benkler</div>
           <div class="italics lato">
@@ -60,7 +32,7 @@ const markup = `
     </div>
     <div class="row find-row">
       <div class="speaker">
-        <img class="speaker-img" src="./images/Speaker5.png">
+        <img class="speaker-img" src="./images/profile.png" alt="third speaker">
         <div class="speaker-info">
           <div class="name lato">Yochai Benkler</div>
           <div class="italics lato">
@@ -73,7 +45,35 @@ const markup = `
         </div>
       </div>
       <div class="speaker">
-        <img class="speaker-img" src="./images/Speaker6.png">
+        <img class="speaker-img" src="./images/profile.png" alt="fourth speaker">
+        <div class="speaker-info">
+          <div class="name lato">Yochai Benkler</div>
+          <div class="italics lato">
+            <em>Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</em>
+          </div>
+          <div class="line2"></div>
+          <div class="background lato">
+            Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row find-row">
+      <div class="speaker">
+        <img class="speaker-img" src="./images/profile.png" alt="fifth speaker">
+        <div class="speaker-info">
+          <div class="name lato">Yochai Benkler</div>
+          <div class="italics lato">
+            <em>Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</em>
+          </div>
+          <div class="line2"></div>
+          <div class="background lato">
+            Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.
+          </div>
+        </div>
+      </div>
+      <div class="speaker">
+        <img class="speaker-img" src="./images/profile.png" alt="sixth speaker">
         <div class="speaker-info">
           <div class="name lato">Yochai Benkler</div>
           <div class="italics lato">
@@ -96,6 +96,9 @@ section.innerHTML = markup;
 
 const moreButton = document.querySelector('.more');
 const hiddenRows = document.querySelectorAll('.find-row');
+const hamburger = document.querySelector('.hamburger');
+const xIcon = document.querySelector('.close');
+const mobileMenu = document.querySelector('.find-menu')
 
 function displayRows() {
   for(let i = 0; i < hiddenRows.length; i += 1) {
@@ -104,4 +107,10 @@ function displayRows() {
   }
 }
 
+function displayMenu() {
+  mobileMenu.classList.toggle('mobile-menu');
+}
+
 moreButton.addEventListener('click', displayRows);
+hamburger.addEventListener('click', displayMenu);
+xIcon.addEventListener('click', displayMenu);
