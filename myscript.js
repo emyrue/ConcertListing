ee4ead1bee4eb5f662b990f8313f15069f2d42e9
emyrue <94935633+emyrue@users.noreply.github.com>
2022-03-08 06:50:11 -0700

    Personalized the first five 'speakers'

--- myscript.js
+++ myscript.js
@@ -1,74 +1,74 @@
 const markup = `
-  <div class="featured-heading lato">Featured Speakers</div>
+  <div class="featured-heading lato">Featured Artists</div>
   <div class="line"></div>
   <div class="speakers">  
     <div class="row visible">
       <div class="speaker">
-        <img class="speaker-img" src="./images/profile.png" alt="first speaker">
+        <img class="speaker-img" src="./images/imagine.png" alt="first artist">
         <div class="speaker-info">
-          <div class="name lato">Yochai Benkler</div>
+          <div class="name lato">Imagine Dragons</div>
           <div class="italics lato">
-            <em>Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</em>
+            <em>American Pop Rock Band from Las Vegas, Nevada</em>
           </div>
           <div class="line2"></div>
           <div class="background lato">
-            Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.
+            Imagine Dragons consists of lead singer Dan Reynolds, guitarist Wayne Sermon, bassist Ben McKee, and drummer Daniel Platzman.
           </div>
         </div>
       </div>
       <div class="speaker">
-        <img class="speaker-img" src="./images/profile.png" alt="second speaker">
+        <img class="speaker-img" src="./images/doja.png" alt="second artist">
         <div class="speaker-info">
-          <div class="name lato">Yochai Benkler</div>
+          <div class="name lato">Doja Cat</div>
           <div class="italics lato">
-            <em>Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</em>
+            <em>American Rapper</em>
           </div>
           <div class="line2"></div>
           <div class="background lato">
-            Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row find-row">
-      <div class="speaker">
-        <img class="speaker-img" src="./images/profile.png" alt="third speaker">
-        <div class="speaker-info">
-          <div class="name lato">Yochai Benkler</div>
-          <div class="italics lato">
-            <em>Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</em>
-          </div>
-          <div class="line2"></div>
-          <div class="background lato">
-            Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.
-          </div>
-        </div>
-      </div>
-      <div class="speaker">
-        <img class="speaker-img" src="./images/profile.png" alt="fourth speaker">
-        <div class="speaker-info">
-          <div class="name lato">Yochai Benkler</div>
-          <div class="italics lato">
-            <em>Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</em>
-          </div>
-          <div class="line2"></div>
-          <div class="background lato">
-            Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.
+            Amala Ratna Zandile Dlamini, known professionally as Doja Cat, is an American rapper, singer, and songwriter. 
           </div>
         </div>
       </div>
     </div>
     <div class="row find-row">
       <div class="speaker">
-        <img class="speaker-img" src="./images/profile.png" alt="fifth speaker">
+        <img class="speaker-img" src="./images/taylor.png" alt="third artist">
         <div class="speaker-info">
-          <div class="name lato">Yochai Benkler</div>
+          <div class="name lato">Taylor Swift</div>
           <div class="italics lato">
-            <em>Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</em>
+            <em>American singer-songwriter</em>
           </div>
           <div class="line2"></div>
           <div class="background lato">
-            Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.
+          Her discography spans multiple genres, and her narrative songwriting, which is often inspired by her personal life, has received widespread media coverage and critical praise.
+          </div>
+        </div>
+      </div>
+      <div class="speaker">
+        <img class="speaker-img" src="./images/okgo.png" alt="fourth artist">
+        <div class="speaker-info">
+          <div class="name lato">OK GO</div>
+          <div class="italics lato">
+            <em>American Rock Band</em>
+          </div>
+          <div class="line2"></div>
+          <div class="background lato">
+            The band is composed of Damian Kulash, Tim Nordwind, Dan Konopka, and Andy Ross, who joined them in 2005, replacing Andy Duncan.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row find-row">
+      <div class="speaker">
+        <img class="speaker-img" src="./images/qveen.png" alt="fifth artist">
+        <div class="speaker-info">
+          <div class="name lato">Qveen Herby</div>
+          <div class="italics lato">
+            <em>American Rapper, Singer, Songwriter, and Entrepreneur</em>
+          </div>
+          <div class="line2"></div>
+          <div class="background lato">
+            Three of her EPs charted within Billboard charts in the US. Herby released a series of singles throughout 2020-2021 which lead to Herby eventually releasing her debut album A Woman on May 21, 2021.
           </div>
         </div>
       </div>
@@ -86,7 +86,7 @@ const markup = `
         </div>
       </div>
     </div>
-    <button class="lato more">More<img src="./images/down.png"></button>
+    <button class="lato default more">More<img src="./images/down.png"></button>
   </div>
 `;
 
@@ -103,7 +103,7 @@ const mobileMenu = document.querySelector('.find-menu')
 function displayRows() {
   for(let i = 0; i < hiddenRows.length; i += 1) {
     hiddenRows[i].classList.add('visible');
-    moreButton.classList.add('invisible');
+    moreButton.classList.remove('more');
   }
 }
 
